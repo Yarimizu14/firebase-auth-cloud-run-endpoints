b4a955790ea21822de2ef4e02ade88a47f03d3f1
Kensei Yarimizu <kensei@abeja.asia>
2019-06-20 00:48:38 +0900

    added firebase-admin and flask-cors

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials
+
+cred_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+if cred_path:
+    cred = credentials.Certificate(cred_path)
+    default_app = firebase_admin.initialize_app(cred)
+else:
+    default_app = firebase_admin.initialize_app()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
